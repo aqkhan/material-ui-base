f417f33b8e364dc75ce55cd69968177cc398489d
A Q Khan <aqk.neo@gmail.com>
2019-07-09 20:50:10 +0500

    Basic Dashboard example

---
+++ src/theme.js
@@ -0,0 +1,22 @@
+import { red } from '@material-ui/core/colors';
+import { createMuiTheme } from '@material-ui/core/styles';
+
+// A custom theme for this app
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#F557D3',
+    },
+    secondary: {
+      main: '#19857b',
+    },
+    error: {
+      main: red.A400,
+    },
+    background: {
+      default: 'black',
+    },
+  },
+});
+
+export default theme;
